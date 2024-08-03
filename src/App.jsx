053423e8fda0293689912ebd148d2c246a9373f7
Dhruv Mehta <flashxx001@gmail.com>
2024-08-03 16:28:36 +0530

    Removed Relative and Absolute for responsiveness.

--- src/App.jsx
+++ src/App.jsx
@@ -22,17 +22,17 @@ function App() {
 // eslint-disable-next-line react/prop-types
 function Advice({ adviceGen, adviceData, count }) {
   return (
-    <div className="h-screen bg-dark-blue flex flex-col gap-4 items-center justify-center relative px-4">
-      <div className="bg-dark-grayish-blue max-w-[370px] text-white p-10 rounded-lg text-center flex flex-col gap-4 min-h-[360px]">
+    <div className="h-screen bg-dark-blue flex flex-col gap-4 items-center justify-center  px-4">
+      <div className="bg-dark-grayish-blue max-w-[370px] text-white p-10 rounded-lg text-center flex flex-col gap-4 ">
         <p className="text-neon-green uppercase tracking-wide">
           A d v i c e #{count}
         </p>
-        <p className="text-2xl font-semibold min-h-[160px]">
+        <p className="text-2xl font-semibold">
           {count === 0
             ? "Click on Dice to Generate Advice."
             : `"${adviceData.advice}"`}
         </p>
-        <div className="relative flex items-center justify-center my-4">
+        <div className=" flex items-center justify-center my-4">
           <div className="flex-grow border-t border-gray-400"></div>
           <div className="mx-2 flex items-center">
             <div className="w-1 h-4 bg-white rounded-sm"></div>
@@ -41,7 +41,7 @@ function Advice({ adviceGen, adviceData, count }) {
           <div className="flex-grow border-t border-gray-400"></div>
         </div>
       </div>
-      <div className="bg-green-400 rounded-full p-2 absolute bottom-28">
+      <div className="bg-green-400 rounded-full p-2">
         <FaDiceFive onClick={adviceGen} className="text-3xl cursor-pointer" />
       </div>
 
